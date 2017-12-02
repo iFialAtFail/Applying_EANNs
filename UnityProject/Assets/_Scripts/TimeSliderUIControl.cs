c6c68887e9fb4b82cc743757338b570d6f5285b6
Michael Manley <mmanle01@live.com>
2017-12-02 13:08:30 -0500

    Added time control to UI and adjusted settings for main scene to support android build.

---
+++ UnityProject/Assets/_Scripts/TimeSliderUIControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSliderUIControl : MonoBehaviour {
+
+    public float timeMin = 1f;
+    public float timeMax = 5f;
+
+	public void OnSliderChanged(float value)
+    {
+        Time.timeScale = ((timeMax-timeMin)*value) + timeMin;
+    }
+
+    private void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            timeMax = 4f; // 5x speed is a little much for my Galaxy S7, so I can assume that to fit most mobile requirements, it should be less than 5x.
+        }
+    }
+}
